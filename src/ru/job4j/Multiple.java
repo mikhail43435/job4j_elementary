c3cd89c0011b43eee31e932538e88060bd831e4a
mikhail43435 <fjkhfesiuhfeshfiusef>
2020-07-12 21:46:25 +0300

    Разбор первой программы. [#325067]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -4,10 +4,7 @@ public class Multiple<x> {
 
     public static void main(String[] args) {
         for (int x = 1; x < 11; x++) {
-
             System.out.println("1 * " + x + " = " + x);
-
         }
-
     }
 }
