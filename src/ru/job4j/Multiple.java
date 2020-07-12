e61e9a83e62877613d719e154af0effe63fbc432
mikhail43435 <fjkhfesiuhfeshfiusef>
2020-07-12 21:44:39 +0300

    Фиксация изменений на github [#325075]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Multiple<x> {
+
+    public static void main(String[] args) {
+        for (int x = 1; x < 11; x++) {
+
+            System.out.println("1 * " + x + " = " + x);
+
+        }
+
+    }
+}
