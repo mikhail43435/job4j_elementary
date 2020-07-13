aaeaa542bee8e927fadc3a4dc459e53bb0daaaa8
mikhail43435 <fjkhfesiuhfeshfiusef>
2020-07-13 21:41:14 +0300

    Разбор первой программы. [#325067]

---
+++ src/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,34 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class Calculator {
+
+
+
+    public static void main(String[] args) {
+
+        int one = 1;
+        int two = 2;
+
+        // создадим дополнительные переменние
+        int four = 4;
+        int five = 5;
+        int six =6;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four - two;
+        //// создали доп переменные
+
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+
+        // выведем созданные доп переменные
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+        //// вывели созданные доп переменные
+
+
+
+    }
+
+}
