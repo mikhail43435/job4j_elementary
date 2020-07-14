a8395cceb1a8ee6f73ed8e73d532d9a6e89f09c4
mikhail43435 <hommp2009@mail.ru>
2020-07-14 19:03:29 +0300

    Идеальный вес [#325047]

--- src/ru/job4j/CalculatorWeight.java
+++ src/ru/job4j/CalculatorWeight.java
@@ -2,13 +2,11 @@ package ru.job4j;
 
 public class CalculatorWeight {
     public static double manWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
@@ -16,10 +14,8 @@ public class CalculatorWeight {
         double man = CalculatorWeight.manWeight(height);
         double woman = CalculatorWeight.womanWeight(height);
 
-        System.out.println("Man 187 cm height ideal weight is " + man );
+        System.out.println("Man 187 cm height ideal weight is " + man);
         System.out.println("Woman 187 cm height ideal weight is " + woman);
 
     }
-
-
 }
