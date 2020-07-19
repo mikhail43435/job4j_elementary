4f4b6d984a8c256ebba1aaee834c1899557d6e7b
mikhail43435 <hommp2009@mail.ru>
2020-07-19 21:17:21 +0300

    Создать программу, вычисляющую факториал. [#325110]

--- src/ru/job4j/cycles/Factorial.java
+++ src/ru/job4j/cycles/Factorial.java
@@ -3,13 +3,12 @@ package ru.job4j.cycles;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-
-        if (n > 1) {
-            for (int index = 2; index <= n; index++) {
-             result = result * index;
-            }
-
+        if (n == 1) {
+            return result;
         }
+        for (int index = 2; index <= n; index++) {
+            result = result * index;
+            }
         return result;
     }
 }
