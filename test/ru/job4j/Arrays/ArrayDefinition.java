2e15151388730f233153741c233ff0dde3932f48
mikhail43435 <hommp2009@mail.ru>
2020-07-19 21:32:11 +0300

    Заполнение массива. [#325032]

--- test/ru/job4j/Arrays/ArrayDefinition.java
+++ test/ru/job4j/Arrays/ArrayDefinition.java
@@ -5,5 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] =  "Nic";
+        names[1] = "Mike";
+        names[2] = "Soul";
+        names[3] = "Trevor";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
