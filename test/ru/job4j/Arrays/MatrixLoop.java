fb612f3288ac34f2830044fa0cf4b6577e6979f7
mikhail43435 <hommp2009@mail.ru>
2020-07-20 20:10:46 +0300

    Двухмерный массив. Циклы. [#325040]

---
+++ test/ru/job4j/Arrays/MatrixLoop.java
@@ -0,0 +1,17 @@
+package ru.job4j.Arrays;
+
+public class MatrixLoop {
+    public static void main(String[] args) {
+        int[][] array = {
+                {4, 2, 3, 454},
+                {7, 1, 5, 56},
+                {3, 5, 6, 67}
+        };
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                int val = array[row][cell];
+                System.out.println(row + "-" + cell + " : " + val);
+            }
+        }
+    }
+}
