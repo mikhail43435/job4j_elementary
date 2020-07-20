bfc872e402b96254cdb49fa5e0ce10ec7c73cd4a
mikhail43435 <hommp2009@mail.ru>
2020-07-20 18:14:49 +0300

    Слово заканчивается на ... [#325024]

---
+++ test/ru/job4j/Arrays/EndsWithTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.Arrays;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenOneSymbol() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void when5Symbols() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'H', 'e', 'r', 'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
