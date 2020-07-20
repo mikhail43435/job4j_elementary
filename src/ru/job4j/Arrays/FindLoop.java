e129681fb028f924ef23287d32e7c1bc4f92df53
mikhail43435 <hommp2009@mail.ru>
2020-07-20 18:25:53 +0300

    Поиск индекса в диапазоне. [#325025]

--- src/ru/job4j/Arrays/FindLoop.java
+++ src/ru/job4j/Arrays/FindLoop.java
@@ -4,8 +4,20 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int index = start; index < finish; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
