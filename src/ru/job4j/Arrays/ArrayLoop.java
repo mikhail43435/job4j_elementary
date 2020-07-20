088c0067ae90578b0b912dc3dc2cb4b9c625f1db
mikhail43435 <hommp2009@mail.ru>
2020-07-20 10:46:00 +0300

    Создать программу, вычисляющую факториал. [#325110]

--- src/ru/job4j/Arrays/ArrayLoop.java
+++ src/ru/job4j/Arrays/ArrayLoop.java
@@ -3,11 +3,11 @@ package ru.job4j.Arrays;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        for (int i = 0; i < numbers.length ; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
         }
         // выводим массив
-        for (int i = 0; i < numbers.length ; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
     }
--- src/ru/job4j/cycles/Factorial.java
+++ src/ru/job4j/cycles/Factorial.java
@@ -3,9 +3,9 @@ package ru.job4j.cycles;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        if (n == 1) {
+        /*if (n == 1) {
             return result;
-        }
+        }*/
         for (int index = 2; index <= n; index++) {
             result = result * index;
             }
