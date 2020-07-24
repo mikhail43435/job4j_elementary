d103d4ac055191eeb12b6bd6e5d310cfa2eb12ea
mikhail43435 <hommp2009@mail.ru>
2020-07-24 22:41:15 +0300

    Переопределение [#325218]

---
+++ src/ru/job4j/oop/inheritance/JSONReport.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        String result = "{" + System.lineSeparator() +
+                "\"name\" : \"" + name + "\"," + System.lineSeparator() +
+                "\"body\" : \"" + body + "\"" + System.lineSeparator() +
+                "}";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        System.out.println(report.generate("Everybody", "Should study Java!"));
+    }
+}
