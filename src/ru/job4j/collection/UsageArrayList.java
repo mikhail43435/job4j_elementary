4254ae9d46500741944430bedb9f952bf0038d28
mikhail43435 <hommp2009@mail.ru>
2020-08-11 13:17:07 +0300

    Коллекции, ArrayList, List, Обобщения [#325138]

---
+++ src/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        al.add(0, "Petr");
+        al.add(1, "Ivan");
+        al.add(2, "Stepan");
+        for (String value : al) {
+            System.out.println(value);
+        }
+        System.out.println("=== With GET");
+        for (int index = 0; index < al.size(); index++) {
+            System.out.println(al.get(index));
+        }
+
+    }
+}
