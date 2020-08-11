0925554a720e7e4bf35f3384760e7d9c6a921a7c
mikhail43435 <hommp2009@mail.ru>
2020-08-11 13:29:49 +0300

    Телефонный справочник на базе ArrayList [#325132]

---
+++ test/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.add(new Person("234", "qsfse", "534872", "234"));
+        ArrayList<Person> persons = phones.find("534872");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+        assertThat(persons.get(1).getSurname(), is("qsfse"));
+    }
+}
