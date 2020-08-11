bae9f6b72baaa3cacf5ebc84a8e15571fda3e5b1
mikhail43435 <hommp2009@mail.ru>
2020-08-11 15:28:26 +0300

    Очередь с приоритетом на LinkedList [#325133] - 2

--- src/ru/job4j/collection/search/PriorityQueue.java
+++ src/ru/job4j/collection/search/PriorityQueue.java
@@ -14,15 +14,12 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() >= task.getPriority()) {
-                this.tasks.add(index, task);
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
             index++;
         }
-        if (index == tasks.size()) {
-            this.tasks.add(index, task);
-        }
+        tasks.add(index, task);
     }
 
     public Task take() {
