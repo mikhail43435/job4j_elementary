178fd1a161fcb2f1a8ba232d5471d9f28234fe53
mikhail43435 <hommp2009@mail.ru>
2020-08-26 15:58:38 +0300

    Сортировка номера [#325127]

---
+++ src/ru/job4j/collection/LexSort.java
@@ -0,0 +1,51 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+
+        if ((left.length() == 0) && (right.length() == 0)) {
+            return 0;
+        }
+
+        if (left.length() == 0) {
+            return -1;
+        }
+        if (right.length() == 0) {
+            return +1;
+        }
+
+        String[] splitLeft = left.split("\\.");
+        String[] splitRight = right.split("\\.");
+
+        int cycleLength = Math.min(splitLeft.length, splitRight.length);
+        for (int index = 0; index < cycleLength; index++) {
+
+            if (isDigit(splitLeft[index]) && isDigit(splitRight[index])) {
+                if (Integer.parseInt(splitLeft[index]) > Integer.parseInt(splitRight[index])) {
+                    return +1;
+                }
+                if (Integer.parseInt(splitLeft[index]) < Integer.parseInt(splitRight[index])) {
+                    return -1;
+                }
+                if (Integer.parseInt(splitLeft[index]) == Integer.parseInt(splitRight[index])) {
+                    return 0;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
---
+++ test/ru/job4j/collection/LexSortTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.util.Arrays;
+
+public class LexSortTest {
+    @Test
+    public void sortNum1and2and10() {
+        String[] input = {
+                "10. Task.",
+                "1. Task.",
+                "2. Task."
+        };
+        String[] out = {
+                "1. Task.",
+                "2. Task.",
+                "10. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+
+    @Test
+    public void sortNum1and2and3() {
+        String[] input = {
+                "3. Task.",
+                "1. Task.",
+                "2. Task."
+        };
+        String[] out = {
+                "1. Task.",
+                "2. Task.",
+                "3. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+
+    @Test
+    public void sortNum1andNulland3() {
+        String[] input = {
+                "3. Task.",
+                "",
+                "2. Task."
+        };
+        String[] out = {
+                "",
+                "2. Task.",
+                "3. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+}
