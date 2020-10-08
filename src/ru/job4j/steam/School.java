8798855055a201b6bdce9e957944f783582936f3
mikhail43435 <hommp2009@mail.ru>
2020-10-08 18:45:39 +0300

    Фильтрация учеников [#324980] - 2

--- src/ru/job4j/steam/School.java
+++ src/ru/job4j/steam/School.java
@@ -7,8 +7,6 @@ import java.util.stream.Collectors;
 public class School {
 
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> selectedStudents;
-        selectedStudents = students.stream().filter(predict).collect(Collectors.toList());
-        return selectedStudents;
+        return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
