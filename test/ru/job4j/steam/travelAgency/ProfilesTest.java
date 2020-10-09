22f06e3f2df8850108ddccf460de51c34d8a9fe6
mikhail43435 <hommp2009@mail.ru>
2020-10-09 15:13:54 +0300

    Уникальность элементов и сортировка. [#324982]3]

--- test/ru/job4j/steam/travelAgency/ProfilesTest.java
+++ test/ru/job4j/steam/travelAgency/ProfilesTest.java
@@ -2,22 +2,13 @@ package ru.job4j.steam.travelAgency;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.steam.School;
-import ru.job4j.steam.Student;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 public class ProfilesTest {
-
     @Test
     public void whenCollectAddresses() {
         Address address1 = new Address("Moscow", "Bannaya", 23, 3);
@@ -35,15 +26,12 @@ public class ProfilesTest {
         Address address1 = new Address("Moscow", "Bannaya", 23, 3);
         Address address2 = new Address("Bryansk", "Lenina", 34, 75);
         Address address3 = new Address("Bryansk", "Lenina", 34, 75);
-
         List<Profile> profileList = new ArrayList<>();
         profileList.add(new Profile(address1));
         profileList.add(new Profile(address2));
         profileList.add(new Profile(address3));
-
         List<Address> addressesFinal = Arrays.asList(address2, address1);
         Profiles profiles = new Profiles();
         assertThat(addressesFinal, is(profiles.collect(profileList)));
     }
-
 }
