29a048f088543db7f0a83398b06eec47c4c0cef1
mikhail43435 <hommp2009@mail.ru>
2020-10-19 15:08:12 +0300

    Слияние двух массивов - 2

--- src/ru/job4j/tests/Merge.java
+++ src/ru/job4j/tests/Merge.java
@@ -1,31 +1,24 @@
 package ru.job4j.tests;
 
+import static java.lang.System.arraycopy;
+
 public class Merge {
 
         public static int[] merge(int[] left, int[] right) {
             int[] rsl = new int[left.length + right.length];
-            int counterLeft = 0;
-            int counterRight = 0;
-            int counterRsl = 0;
+            int counterLeft = 0, counterRight = 0, counterRsl = 0;
 
-            while (counterRsl < rsl.length) {
-                if (counterLeft == left.length) {
-                    for (int index = counterRight; index < right.length; index++) {
-                        rsl[counterRsl++] = right[index];
-                    }
-                    return rsl;
-                }
-                if (counterRight == right.length) {
-                    for (int index = counterLeft; index < left.length; index++) {
-                        rsl[counterRsl++] = left[index];
-                    }
-                    return rsl;
-                }
+            while (counterLeft < left.length && counterRight < right.length) {
                 if (left[counterLeft] <= right[counterRight]) {
                     rsl[counterRsl++] = left[counterLeft++];
                 } else {
                     rsl[counterRsl++] = right[counterRight++];
                 }
+            }
+            if (counterLeft < left.length) {
+                arraycopy(left, counterLeft, rsl, counterRsl, left.length -  counterLeft);
+            } else if (counterRight < right.length) {
+                arraycopy(right, counterRight, rsl, counterRsl, right.length -  counterRight);
             }
             return rsl;
         }
