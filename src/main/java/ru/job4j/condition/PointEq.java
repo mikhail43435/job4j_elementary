96f3ccb99100567604b84cd634c628246f95e69b
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-05-24 17:37:51 +0300

    if c return [#333785]

---
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class PointEq {
+
+    public static boolean paarEq(int x1, int y1, int x2, int y2) {
+        return (x1 == x2) && (y1 == y2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(paarEq(1, 1, 2, 2));
+        System.out.println(paarEq(10, 10, 10, 10));
+        System.out.println(paarEq(11, 10, 11, 10));
+    }
+}
